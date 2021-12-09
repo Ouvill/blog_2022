a67c81d9a8609dc3e7f0095a9887026bf0ff8905
Ouvill <contact@ouvill.net>
2021-12-10 00:39:38 +0900

    copy blog posts

---
+++ blog/2019-04-14--learn-typescript/src/greeter1.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function greeter(person) {
+  return "Hello, " + person;
+}
+
+var user = "Jane User";
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=greeter1.js.map
---
+++ blog/2019-04-14--learn-typescript/src/greeter2.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function greeter(person) {
+  return "Hello, " + person;
+}
+
+var user = [0, 1, 2];
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=greeter2.js.map
