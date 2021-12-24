c41b2f38489421efa1998f2dfa43537cec399d34
Ouvill <contact@ouvill.net>
2021-12-24 16:07:31 +0900

    create Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <footer>© 2022,Designed and Written by Ouvill ,Built with Gatsby</footer>
+  );
+};
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
       <Header />
       {children}
+      <Footer />
     </div>
   );
 };
