b17b7ddf8000496ccce3654068a907fb8a5c2a13
Ouvill <contact@ouvill.net>
2021-12-24 18:06:17 +0900

    add category index page

--- gatsby-node.js
+++ gatsby-node.js
@@ -86,9 +86,52 @@ const createBlogPage = async (actions, graphql) => {
   });
 };
 
+const createCategoryIndexPage = async (actions, graphql) => {
+  const { createPage } = actions;
+  const categoryIndexTemplate = require.resolve(
+    `./src/templates/CategoryIndexTemplate.tsx`
+  );
+  console.log("createCategoryIndexPage");
+  return graphql(`
+    query CreateCategoryIndexPage {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "//blog//" } }
+        sort: { order: DESC, fields: [frontmatter___date] }
+      ) {
+        group(field: frontmatter___category) {
+          fieldValue
+          totalCount
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+    const postsPerPage = 10;
+    result.data.allMarkdownRemark.group.forEach((category) => {
+      const numPages = Math.ceil(category.totalCount / postsPerPage);
+      Array.from({ length: numPages }).forEach((_, i) => {
+        createPage({
+          path: `/categories/${category.fieldValue}/${i + 1}`,
+          component: categoryIndexTemplate,
+          context: {
+            limit: postsPerPage,
+            skip: i * postsPerPage,
+            numPages,
+            currentPage: i + 1,
+            category: category.fieldValue,
+          },
+        });
+      });
+    });
+  });
+};
+
 exports.createPages = async ({ actions, graphql }) => {
   return Promise.all([
     createBlogPage(actions, graphql),
     createBlogIndexPage(actions, graphql),
+    createCategoryIndexPage(actions, graphql),
   ]);
 };
