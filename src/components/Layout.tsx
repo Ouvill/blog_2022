24f4b3fce790b3559dfa891bf081a48dd603c1cc
Ouvill <contact@ouvill.net>
2021-12-29 20:35:40 +0900

    メインコンテンツを伸ばす

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Main = styled.main`
+  flex: 1;
+`;
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <Wrapper>
       <Header />
-      <main>{children}</main>
+      <Main>{children}</Main>
       <Footer />
-    </div>
+    </Wrapper>
   );
 };
 
